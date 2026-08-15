90509c99d3909a9b74f83e7a40878c85937c2787
pauloecb <abelhinha.ia@gmail.com>
2026-08-15 14:50:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -31,11 +31,10 @@ st.markdown("Descubra o raio de alcance das abelhas nativas a partir da sua loca
 
 # Dicionário completo de espécies e raios em ordem alfabética
 especies_abelhas = {
-    "Apis mellifera (Abelha com ferrão / Europa/Africana)": 2500,
+    "Jataí-Acriana (Tetragonisca weyrauchi)":500,
     "Boiassu (Melipona interrupta)": 1500,
     "Bugia (Melipona bicolor)": 1000,
     "Canudo (Scaptotrigona depilis)": 1500,
-    "Cephalotrigona capitata (Mombucão)": 1000,
     "Guaraipo (Melipona bicolor schencki)": 1000,
     "Guiruçu (Schwarziana quadripunctata)": 1000,
     "Iraí (Nannotrigona testaceicornis)": 500,
